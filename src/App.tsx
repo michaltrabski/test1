d6c7d77b703984e2e556c648843bcf2f46990944
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-10-20 10:31:48 +0200

    summary michal 1 optional desc michal 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Test1 CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! zmiana 3</h2>
+      <h2>Start editing to see some magic happen! zmiana 4</h2>
     </div>
   );
 }
