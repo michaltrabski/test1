c2ca3dd51e650681258b4ff8c0dfb7ad22aa93ff
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-10-20 10:38:59 +0200

    update 3 some description to update 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ export default function App() {
       <h1>Test1 CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>Wersia 1</p>
-      
-              <p>Wersia 2</p>
+      <p>Wersia 2</p>
+      <p>Wersia 3</p>
     </div>
   );
 }
