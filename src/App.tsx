9ab912202f44f491a48d9ac4ac3f264b5bfb5e8e
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-10-20 10:37:04 +0200

    aaa bbb

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Test1 CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! zmiana 4</h2>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>Wersia 1</p>
     </div>
   );
 }
